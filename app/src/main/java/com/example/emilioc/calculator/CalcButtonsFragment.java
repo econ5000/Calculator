686c3eb3930ae17a1c2702bc3fea4b5f2f320f9f
econ5000 <econ5000@hotmail.com>
2015-08-14 17:33:43 -0700

    updates to UI, including adding fragment for clac input buttons

---
+++ app/src/main/java/com/example/emilioc/calculator/CalcButtonsFragment.java
@@ -0,0 +1,29 @@
+package com.example.emilioc.calculator;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CalcButtonsFragment extends Fragment {
+
+
+    public CalcButtonsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_input_buttons, container, false);
+    }
+
+
+}
